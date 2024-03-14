501fd1c894d5525c6eb2916324f65bdc2d2f3a2d
CarlosAkel <cmakel@miuandes.cl>
2024-03-13 23:32:41 -0300

    checked auth

--- back_end/main.py
+++ back_end/main.py
@@ -256,6 +256,7 @@ async def auth(request: Request, db: Session = Depends(get_db)):
     try:
         user = db.query(models.User).filter(models.User.email == user_data.email).first()
         user2 = db.query(models.User).filter(models.User.username == user_data.name).first()
+        json_data = jsonable_encoder(user_data)
         if not user and not user2:
             random_password = secrets.token_urlsafe(16)
             new_user = models.User(username=user_data.name, email=user_data.email, password=random_password)
@@ -268,12 +269,20 @@ async def auth(request: Request, db: Session = Depends(get_db)):
             access_token = create_access_token(
                 data={"sub": user_data.name}, expires_delta=access_token_expires
             )
-            response = RedirectResponse(url="https://carlosakel.github.io/urls")  # Change "/logged" to your actual logged page URL
+            json_data = jsonable_encoder(Token(access_token=access_token, token_type="bearer"))
+            response = JSONResponse(content=json_data)
             response.set_cookie(key="access_token", value=access_token)
             return response
     except Exception as error:
         return f"Error: {error}"
 
+@app.get("/check-authentication-status", tags=["CORS"])
+async def check_authentication_status(request: Request):
+    access_token = request.cookies.get("access_token")
+    if access_token:
+        return {"authenticated": True}
+    else:
+        return {"authenticated": False}
 
 # @app.get('/google/logout')
 # def logout(request: Request):
